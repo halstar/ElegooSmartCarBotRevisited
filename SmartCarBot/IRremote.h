b60efc115049b0d3fd753a5cf65b8bb557ff1f46
Hal Star <halstar@hotmail.com>
2019-01-24 18:41:51 +0100

    Removed unecessary IR control files

--- SmartCarBot/IRremote.h
+++ SmartCarBot/IRremote.h
@@ -28,53 +28,53 @@
 // Each protocol you include costs memory and, during decode, costs time
 // Disable (set to 0) all the protocols you do not need/want!
 //
-#define DECODE_RC5           1
-#define SEND_RC5             1
+#define DECODE_RC5           0
+#define SEND_RC5             0
 
-#define DECODE_RC6           1
-#define SEND_RC6             1
+#define DECODE_RC6           0
+#define SEND_RC6             0
 
 #define DECODE_NEC           1
-#define SEND_NEC             1
+#define SEND_NEC             0
 
-#define DECODE_SONY          1
-#define SEND_SONY            1
+#define DECODE_SONY          0
+#define SEND_SONY            0
 
-#define DECODE_PANASONIC     1
-#define SEND_PANASONIC       1
+#define DECODE_PANASONIC     0
+#define SEND_PANASONIC       0
 
-#define DECODE_JVC           1
-#define SEND_JVC             1
+#define DECODE_JVC           0
+#define SEND_JVC             0
 
-#define DECODE_SAMSUNG       1
-#define SEND_SAMSUNG         1
+#define DECODE_SAMSUNG       0
+#define SEND_SAMSUNG         0
 
-#define DECODE_WHYNTER       1
-#define SEND_WHYNTER         1
+#define DECODE_WHYNTER       0
+#define SEND_WHYNTER         0
 
-#define DECODE_AIWA_RC_T501  1
-#define SEND_AIWA_RC_T501    1
+#define DECODE_AIWA_RC_T501  0
+#define SEND_AIWA_RC_T501    0
 
-#define DECODE_LG            1
-#define SEND_LG              1 
+#define DECODE_LG            0
+#define SEND_LG              0 
 
-#define DECODE_SANYO         1
+#define DECODE_SANYO         0
 #define SEND_SANYO           0 // NOT WRITTEN
 
-#define DECODE_MITSUBISHI    1
+#define DECODE_MITSUBISHI    0
 #define SEND_MITSUBISHI      0 // NOT WRITTEN
 
 #define DECODE_DISH          0 // NOT WRITTEN
-#define SEND_DISH            1
+#define SEND_DISH            0
 
 #define DECODE_SHARP         0 // NOT WRITTEN
-#define SEND_SHARP           1
+#define SEND_SHARP           0
 
-#define DECODE_DENON         1
-#define SEND_DENON           1
+#define DECODE_DENON         0
+#define SEND_DENON           0
 
 #define DECODE_PRONTO        0 // This function doe not logically make sense
-#define SEND_PRONTO          1
+#define SEND_PRONTO          0
 
 //------------------------------------------------------------------------------
 // When sending a Pronto code we request to send either the "once" code
